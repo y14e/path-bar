cc4f23f263b03cd1930886ea9cc81ae16b7e866f
Yusuke Kamiyamane <y@y14e.com>
2026-06-28 20:24:11 +0900

    Rename

---
+++ update-dependencies.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+call npm un power-focusable @y14e/attributes-utils @y14e/menu @y14e/roving-tabindex
+call npm i power-focusable @y14e/attributes-utils @y14e/menu @y14e/roving-tabindex
